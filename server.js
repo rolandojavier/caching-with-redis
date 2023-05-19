40d1faf7977c1a0c229fd3be8dbddb980eacef12
Rolando Ramos <rolando.ramos@intechideas.com>
2023-05-19 15:42:35 -0500

    Beautify

--- server.js
+++ server.js
@@ -1,61 +1,61 @@
-const express = require('express')
-const { createClient } = require('redis')
-const { setTimeout } = require('timers/promises')
+const express = require("express");
+const { createClient } = require("redis");
+const { setTimeout } = require("timers/promises");
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-const client = createClient({url: 'redis://redis:6379'})
-client.on('error', (err) => console.log('Redis Client Error', err))
+const client = createClient({ url: "redis://redis:6379" });
+client.on("error", (err) => console.log("Redis Client Error", err));
 
 const slowQuery = async () => {
-  await setTimeout(3000)
-  return { type: 'cars', amount: 15 }
-}
+  await setTimeout(3000);
+  return { type: "cars", amount: 15 };
+};
 
-app.get('/data', async (_req, res) => {
-  const data = await slowQuery()
-  res.send(data)
-})
+app.get("/data", async (_req, res) => {
+  const data = await slowQuery();
+  res.send(data);
+});
 
-app.get('/data_fast', async (_req, res) => {
+app.get("/data_fast", async (_req, res) => {
   // Open connection to Redis
   await client.connect();
 
-  let data
+  let data;
   // Look for data in Redis
-  data = JSON.parse(await client.get('data'));
+  data = JSON.parse(await client.get("data"));
 
-  if(!data) {
+  if (!data) {
     // If it is a miss we get the data again
-    data = await slowQuery()
+    data = await slowQuery();
     // We save the data in Redis for subsequent requests
-    await client.set('data', JSON.stringify(data));
+    await client.set("data", JSON.stringify(data));
   }
 
   // We close the connection to Redis
   await client.disconnect();
 
-  res.send(data)
-})
+  res.send(data);
+});
 
-app.get('/data_fresh', async (_req, res) => {
+app.get("/data_fresh", async (_req, res) => {
   await client.connect();
 
-  let data
-  data = JSON.parse(await client.get('data'));
+  let data;
+  data = JSON.parse(await client.get("data"));
 
-  if(!data) {
-    data = await slowQuery()
+  if (!data) {
+    data = await slowQuery();
     // We save the data in Redis only for 20 seconds
-    await client.setEx('data', 20, JSON.stringify(data));
+    await client.setEx("data", 20, JSON.stringify(data));
   }
 
   await client.disconnect();
 
-  res.send(data)
-})
+  res.send(data);
+});
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
-})
+  console.log(`App listening on port ${port}`);
+});
